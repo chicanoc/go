69fbf11d734ebbb328630d635e07b6080f07ff54
chicanoc <chek2505@gmail.com>
2023-09-03 00:54:51 +0500

    my-branch2

--- src/Main.java
+++ src/Main.java
@@ -11,11 +11,11 @@ public class Main {
 
             for (int factor = 0; factor <= value; factor++) {
 
-                for (int factor2 = 0; factor2 <= value; factor2++) {
+                for (int factor2 = 0; factor3 <= value; factor2++) {
 
-                    if (factor * factor2 == value) {
+                    if (factor * factor3 == value) {
 
-                        System.out.println(factor + "*" + factor2);
+                        System.out.println(factor + "*" + factor3);
                     }
                 }
             }
